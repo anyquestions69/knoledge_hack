d58cf4f2a0461021f4473bda04f272b3235ce1db
public_hysteria <juuzodes@yandex.ru>
2024-04-23 23:26:45 +0300

    added static

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Get, Post, Render } from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -6,7 +6,12 @@ export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Render('index')
+  root() {
+    return ;
+  }
+  @Post('check')
+  async name(@Body()text:string) {
+    return await this.appService.check(text);
   }
 }
