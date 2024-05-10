ab7e66ff12cc69956b9ab469e879f1853655e17a
public_hysteria <juuzodes@yandex.ru>
2024-05-10 19:23:45 +0300

    edit gitignore

--- machine/main.py
+++ machine/main.py
@@ -17,6 +17,7 @@ channel.queue_declare(queue='pupupu')
 def on_request(ch, method, props, body):
    
     text = json.loads(body.decode('utf-8'))
+    print(text)
     response = Tyrenko.determined_text_to_title(text.title, text.text)
     ch.basic_publish(exchange='',
                     routing_key=props.reply_to,
