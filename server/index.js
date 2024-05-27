3e1a1d1bdd81c40d874f21e5cd7eca7ef30b6d43
public_hysteria <juuzodes@yandex.ru>
2024-05-27 17:27:01 +0300

    f

--- server/index.js
+++ server/index.js
@@ -1,7 +1,9 @@
 const { WebSocketServer } = require('ws');
 const http = require('http');
 const express= require('express')
+const cors = require('cors');
 const app = express()
+app.use(cors())
 const server = http.createServer();
 const wsServer = new WebSocketServer({ server });
 const port = 8000;
